cb4c13b05374df1e62e027ffe9251b273bf0fada
emmRas01 <emmras01@easv365.dk>
2025-11-25 10:58:59 +0100

    add Triangle and getArea

---
+++ src/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle
+{
+    public int h = 10;
+    public int g = 5;
+
+    public boolean getArea()
+    {
+        double area = ((double) 1/2) * h * g;
+        System.out.println("Arealet af trekanten er " + area);
+        return false;
+    }
+}
